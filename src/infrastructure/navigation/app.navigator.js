7131af46b403d32be542487e72cf4db3b815f8e9
Bramstorp <m.bramstorp@stofanet.dk>
2021-03-21 19:53:53 +0100

    added base theme + base navigation

---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { View, Text } from "react-native";
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Homescreen: "md-home",
+};
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+export const AppNavigator = () => (
+    <Tab.Navigator
+      screenOptions={createScreenOptions}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <Tab.Screen name="Homescreen" component={HomeScreen} />
+    </Tab.Navigator>
+);
