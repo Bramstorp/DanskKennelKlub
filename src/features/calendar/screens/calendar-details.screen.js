0ab0ba81014cd73ff3a6f0298205fbda154b0aec
Bramstorp <m.bramstorp@stofanet.dk>
2021-03-23 00:58:20 +0100

    added test styled

--- src/features/calendar/screens/calendar-details.screen.js
+++ src/features/calendar/screens/calendar-details.screen.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
+import { Test } from "./calendar-style"
 
 export const CalendarDetailScreen = ({ route }) => {
     const { calendar } = route.params;
 
     return (
-        <SafeArea>
+        <Test>
             <Text>Detail test screen</Text>
-            <Text>Name: {JSON.stringify(calendar.name)}</Text>
-            <Text>itemId: {JSON.stringify(calendar.id)}</Text>
-        </SafeArea>
+            <Text>Name: {calendar.name}</Text>
+            <Text>itemId: {calendar.id}</Text>
+        </Test>
     );
 };
-
