95334fb5a5bc064b010580d494e0e1abfee0673b
Bramstorp <m.bramstorp@stofanet.dk>
2021-03-23 18:58:51 +0100

    added account screen full setup with context

--- App.js
+++ App.js
@@ -5,6 +5,21 @@ import { ThemeProvider } from "styled-components/native";
 import { Navigation } from "./src/infrastructure/navigation";
 import { theme } from "./src/infrastructure/theme";
 
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSTaSuyPoT6sOrVeX_usYxhbhWt4qEKHc",
+  authDomain: "meals-to-go-test.firebaseapp.com",
+  projectId: "meals-to-go-test",
+  storageBucket: "meals-to-go-test.appspot.com",
+  messagingSenderId: "678774396998",
+  appId: "1:678774396998:web:3fced6763f5473aa435247",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 export default function App() {
   return (
     <>
