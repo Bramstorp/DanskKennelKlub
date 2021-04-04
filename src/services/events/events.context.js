6b5b9147451dcf3585fb165ed2e9fe5a6d76c7de
Bramstorp <m.bramstorp@stofanet.dk>
2021-04-04 19:02:41 +0200

    syntax update for events

--- src/services/events/events.context.js
+++ src/services/events/events.context.js
@@ -4,30 +4,61 @@ import { eventsRequest } from "./events.service";
 
 export const EventsContext = createContext();
 
+import * as firebase from "firebase";
+
+import moment from "moment";
+
 export const EventsContextProvider = ({ children }) => {
-  const [date, setdate] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [isEventLoading, setEventIsLoading] = useState(false);
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    setIsLoading(true);
-    eventsRequest()
-      .then((results) => {
-        setIsLoading(false);
-        setdate(results);
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        setError(err);
+    firebase
+      .database()
+      .ref("calendar")
+      .once("value")
+      .then((snapshot) => {
+        let values = [];
+        snapshot.forEach((child) => {
+          values.push(child.val());
+        });
+        setEvents(values);
       });
   }, []);
+
+  const setEvent = (date, name, eventName) => {
+    setEventIsLoading(true);
+    const dateFormat = date.format(moment.HTML5_FMT.DATE);
+    const dateKeyParam = dateFormat;
+
+    if (events.length === 0) {
+      events.push({
+        [dateFormat]: [{ name: name, eventName: eventName }],
+      });
+    } else {
+      Object.entries(events[0]).forEach(([key]) => {
+        if (key === dateKeyParam) {
+          events[0][key].push({ name: name, eventName: eventName });
+        } else {
+          const data = { [dateFormat]: [{ name: name, eventName: eventName }] };
+          Object.assign(events[0], data);
+        }
+      });
+    }
+    firebase
+      .database()
+      .ref("calendar/events")
+      .update(events[0])
+      .then(function () {
+        setEventIsLoading(false);
+      });
+  };
 
   return (
     <EventsContext.Provider
       value={{
-        isLoading,
-        error,
-        date,
+        setEvent,
+        isEventLoading,
       }}
     >
       {children}
