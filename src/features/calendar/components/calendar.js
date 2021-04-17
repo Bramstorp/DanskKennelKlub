5e5fbc3419381debcfceb671c5865a27f3e1d476
Mathias Bramstorp <m.bramstorp@stofanet.dk>
2021-04-17 20:26:31 +0200

    added calendar to own component

---
+++ src/features/calendar/components/calendar.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { TouchableOpacity, View } from "react-native";
+
+import { Agenda } from "react-native-calendars";
+
+import { Text } from "../../../components/typography/text.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { EventCard, EmptyDate } from "../components/calendar-style";
+
+export const Calendar = ({ date, navigation }) => {
+  const renderItem = (date) => {
+    return (
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("CalendarDetail", {
+            calendar: date,
+          })
+        }
+      >
+        <EventCard>
+          <Text>
+            {date.starttime} - {date.endtime}
+          </Text>
+          <Spacer size="medium">
+            <Text>{date.name}</Text>
+          </Spacer>
+          <Spacer size="medium">
+            <Text>{date.eventName}</Text>
+          </Spacer>
+        </EventCard>
+      </TouchableOpacity>
+    );
+  };
+
+  const emptyDate = () => {
+    return (
+      <EmptyDate>
+        <Text>This is empty date!</Text>
+      </EmptyDate>
+    );
+  };
+
+  const emptyData = () => {
+    return (
+      <EmptyDate>
+        <Text>This is empty date!</Text>
+      </EmptyDate>
+    );
+  };
+
+  return (
+    <Agenda
+      items={date}
+      renderItem={renderItem}
+      pastScrollRange={5}
+      futureScrollRange={5}
+      renderEmptyDate={emptyDate}
+      renderEmptyData={emptyData}
+      theme={{
+        agendaDayTextColor: "#d9534f",
+        agendaTodayColor: "#d9534f",
+        agendaKnobColor: "#d9534f",
+        selectedDayBackgroundColor: "#d9534f",
+        dotColor: "#d9534f",
+      }}
+    />
+  );
+};
--- src/features/calendar/screens/calendar.screen.js
+++ src/features/calendar/screens/calendar.screen.js
@@ -1,23 +1,17 @@
-import React, { useContext, useState, useEffect } from "react";
-import { TouchableOpacity } from "react-native";
-import { useFocusEffect } from '@react-navigation/native';
+import React, { useContext, useState, useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
-import { Spacer } from "../../../components/spacer/spacer.component";
-import { EventCard, EmptyDate } from "../components/calendar-style";
-import { Agenda } from "react-native-calendars";
-import { Text } from "../../../components/typography/text.component";
-import { CalendarContext } from "../../../services/calendar/calendar.context";
+
+import { Calendar } from "../components/calendar";
 
 import * as firebase from "firebase";
 
 export const CalendarScreen = ({ navigation }) => {
   const [date, setDate] = useState([]);
 
-  console.log(date)
-
   useFocusEffect(
-    React.useCallback(() => {
+    useCallback(() => {
       const ref = firebase.database().ref("calendar/events");
       ref.once("value").then((snapshot) => {
         let result = {};
@@ -27,69 +21,14 @@ export const CalendarScreen = ({ navigation }) => {
             result[daySnapshot.key].push(eventSnapshot.val());
           });
         });
-        setDate(result)
+        setDate(result);
       });
     }, [date.date])
   );
 
-  const renderItem = (date) => {
-    return (
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate("CalendarDetail", {
-            calendar: date,
-          })
-        }
-      >
-        <EventCard>
-          <Text>
-            {date.starttime} - {date.endtime}
-          </Text>
-          <Spacer size="medium">
-            <Text>{date.name}</Text>
-          </Spacer>
-          <Spacer size="medium">
-            <Text>{date.eventName}</Text>
-          </Spacer>
-        </EventCard>
-      </TouchableOpacity>
-    );
-  };
-
-  const emptyDate = () => {
-    return (
-      <EmptyDate>
-        <Text>This is empty date!</Text>
-      </EmptyDate>
-    );
-  };
-
-  const emptyData = () => {
-    return (
-      <EmptyDate>
-        <Text>This is empty date!</Text>
-      </EmptyDate>
-    );
-  };
-
-
   return (
     <SafeArea>
-        <Agenda
-          items={date}
-          renderItem={renderItem}
-          pastScrollRange={5}
-          futureScrollRange={5}
-          renderEmptyDate={emptyDate}
-          renderEmptyData={emptyData}
-          theme={{
-            agendaDayTextColor: "#d9534f",
-            agendaTodayColor: "#d9534f",
-            agendaKnobColor: "#d9534f",
-            selectedDayBackgroundColor: "#d9534f",
-            dotColor: "#d9534f",
-          }}
-        />
+      <Calendar date={date} navigation={navigation} />
     </SafeArea>
   );
 };
